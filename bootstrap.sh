8cfbf66d5aa0a0a91f014dc39cd2f80e6f8bf142
CK <chris.keith@gmail.com>
2023-09-27 11:20:35 -0700

    Java package to old

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,10 @@ fi
 mkdir -p ~/Documents/Github                                     ; if [ $? -ne 0 ] ; then exit 66 ; fi
 cd ~/Documents/Github                                           ; if [ $? -ne 0 ] ; then exit 66 ; fi
 rm -rf JParticle/                                               ; if [ $? -ne 0 ] ; then exit 66 ; fi
-git clone https://github.com/Walter-Stroebel/JParticle.git      ; if [ $? -ne 0 ] ; then exit 66 ; fi
 git clone https://github.com/chrisxkeith/stove-monitor-app.git  ; if [ $? -ne 0 ] ; then exit 66 ; fi
-cp -R JParticle/src/* stove-monitor-app/app/src/                ; if [ $? -ne 0 ] ; then exit 66 ; fi
+# Doesn't build under ??? version of Java
+# git clone https://github.com/Walter-Stroebel/JParticle.git      ; if [ $? -ne 0 ] ; then exit 66 ; fi
+# cp -R JParticle/src/* stove-monitor-app/app/src/                ; if [ $? -ne 0 ] ; then exit 66 ; fi
 
 if [ ! -f ~/Documents/particle-token.txt ] ; then
 	echo "Please create ~/Documents/particle-token.txt containing your Particle token."
